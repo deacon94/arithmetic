fbb4475fa21b8e2123b0cee398da5d2750fa8461
deacon94 <mrich1834@gmail.com>
2021-04-04 19:54:23 -0400

    Initialization

---
+++ arithmetic_arranger.py
@@ -0,0 +1,9 @@
+def arithmetic_arranger(problems,solve):
+
+    # Figure out the problems provided
+    # arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
+
+
+    arranged_problems = ''
+
+    return arranged_problems
